47ffa7fa5fa3a6c108ef11d1f325479d68b9ea56
alexsoares4a <73480812+alexsoares4a@users.noreply.github.com>
2021-10-02 18:53:00 -0300

    Curso de Django 2 #04: URLs, Views e Templates

--- todo/tasks/views.py
+++ todo/tasks/views.py
@@ -4,3 +4,8 @@ from django.http import HttpResponse
 def helloWorld(request):
     return HttpResponse('Hello World!')
 
+def taskList(request):
+    return render(request, 'tasks/list.html')
+
+def yourName(request, name):
+    return render(request, 'tasks/yourname.html', {'name': name})
